6748649780c835adc1631aab0be026d5193dd56e
flying-solo <flyingsoloharshit@gmail.com>
2023-03-19 15:10:52 +0530

    hello route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { loginUser, me, registerUser } from "../controller/user";
+import {loginUser, me, registerUser } from "../controller/user";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/me", me);
+router.get("/hello", (req, res) => res.send("Hello World!"));
 
 module.exports = router;
